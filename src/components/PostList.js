d7986377e390265d5c3ff02fc0ed171e92bb5627
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-07 14:01:33 -0700

    import connect and fetchPosts, build lifecycle, set up connect()

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 //class-based to have access to lifecycle methods
 //componentDidMount will have an action creator
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts()
+  }
+
   render() {
     return (
       <div>
@@ -12,4 +18,6 @@ class PostList extends React.Component {
   }
 }
 
-export default PostList
+//pass in a 1st argument of mapStateToProps
+//right now it isn't present(yet) therefore pass in null
+export default connect(null, { fetchPosts })(PostList)
