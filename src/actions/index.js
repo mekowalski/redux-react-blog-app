4f0485ad0a0ebfe116ce6049e95a4dfe265b5e3a
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-08 13:47:55 -0700

    explain redux-thunk process

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  return function() {
+  return function(dispatch, getState) {
     const promise = jsonPlaceholder.get('/posts');
 
     return {
@@ -14,4 +14,6 @@ export const fetchPosts = () => {
 
 //L5 can return a function that has some logic inside of it
 //how does this help?
-//
+//Redux-Thunk will invoke the returned function and passes in dispatch and getState functions as arguments
+//dispatch is the same function that passes actions as arg, to reducers(change data)
+//getState can be called on Redux Store to return all data/state in Store(read/access data)
