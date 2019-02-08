4f76af9d071a5b777e23428f19c8d417f0b9e687
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-08 13:44:47 -0700

    explain new code for actions, beginning redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,16 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get('/posts');
+  return function() {
+    const promise = jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: promise
-  };
+    return {
+      type: 'FETCH_POSTS',
+      payload: promise
+    }
+  }
 }
 
-//this looks correct
-//have an action creator that makes a request and return an action object with data on payload property
-//BUT THIS IS NOT CORRECT CODE, THIS IS BAD APPROACH
-//Breaking Redux rules, of action creator
-//ACTIONS MUST BE PLAIN OBJECTS
-
-//promise version is also not great
+//L5 can return a function that has some logic inside of it
+//how does this help?
+//
