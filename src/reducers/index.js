b6c2255c17695c08b331c9c8dcc2d0c858456878
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-13 10:54:38 -0700

    minor reducer notes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,3 +4,5 @@ import postsReducer from './postsReducer'
 export default combineReducers({
   posts: postsReducer
 })
+
+//separate file for postsReducer
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,5 @@
 export default () => {
   return 369
 }
+
+//L2: Rule1-Must return ANY value other than 'undefined'/Should always have a return statement with value
