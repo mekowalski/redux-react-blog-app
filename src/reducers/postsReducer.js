18916a81fcf2d9b1fb550733c5c80f6d9b71bf3a
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-13 15:07:28 -0700

    refactor from IF to SWITCH statement

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,16 @@
 export default (state = [], action) => {
-  if (action.type === 'FETCH_POSTS') {
-    return action.payload
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload
+    default:
+      return state
   }
-  return state
 }
 
 //postsReducer handles list of posts therefore default state to an empty array
 //reducer will inspect action type, then return the payload
 //handle case for not finding appropriate action type, always return state, not undefined from a reducer
+
+//this is NOT a normal reducer, many times a reducer will handle multiple action types with a SWITCH
+//statement opposed to an if statement
+//switch() is valid JS, not React
