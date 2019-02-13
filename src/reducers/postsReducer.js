4ef33aedb192e5eaa50424dcb6944af46581e504
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-13 12:04:27 -0700

    minor last rule for reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -5,3 +5,4 @@ export default () => {
 //L2: Rule1-Must return ANY value other than 'undefined'/Should always have a return statement with value
 //Rule2-Provide state/data using only previous state value that was returned and action
 //Rule3-Must not react our of its own function to decide what value to return(pure reducers)
+//Rule4-Must not mutate input state argument
