80566af1d3a821a9ca8d347b69046c89e4682da3
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-14 15:12:32 -0700

    remove comments and modify action dispatch for only response data

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,9 @@ export const fetchPosts = () => {
   return async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
   }
 }
+
+//L8: entire response is dispacthed when only the data tab should be dispatched
+//modify dispatch for payload: response.data
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -11,17 +11,7 @@ class PostList extends React.Component {
   render() {
     console.log(this.props.posts)
     {
-      //returned 2 console logs
-      //when app is first loaded, all reducers run 1 inital time
-      //the action in the reducer won't be matched therefore the state will be returned with default of []
-      //react side of app will render 1 inital time
-      //during inital 1 time, the render method will call & invoke console log which is the first [] array
-      //after PostList component is rendered, componentDidMount will be called
-      //and the entire process will begin again
-      //which then will return the value of the payload property
-      //Redux will see that the the state object is not the same in memory, returned new value(payload)
-      //Redux essentially tells React to re-render itself with new data
-      //therefore PostList component is rendered a 2nd time (HENCE: 2 console logs)
+      //the only attribute in the console log is data tab
     }
     return (
       <div>
