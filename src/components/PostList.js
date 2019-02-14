2337108a8c7b012cf92f215dcc7c4d4e4f417ba5
Malind Kowalski <malind.nikulski@gmail.com>
2019-02-14 14:58:30 -0700

    explain cycle of receiving posts object on PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
+//3. that object will show up as the PROPs object in the PostList component
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts()
@@ -18,8 +19,11 @@ class PostList extends React.Component {
 
 //define mapStateToProps
 //state will have property of posts and that property will hold all data retruned from Reducer
+//2. return that new object with property posts
 const mapStateToProps = state => {
-
+  return { posts: state.posts }
 }
 
-export default connect(null, { fetchPosts })(PostList)
+//take mapStateToProps and hand it in as connect() first argument
+//1. every time the reducer is run, mapStateToProps will be called again
+export default connect(mapStateToProps, { fetchPosts })(PostList)
