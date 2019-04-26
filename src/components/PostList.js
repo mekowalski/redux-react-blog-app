b2ef146f804fcc1a5af8672272b76f4d22e94438
Malind Kowalski <malind.nikulski@gmail.com>
2019-04-26 15:09:56 -0600

    remove comments and began helper method

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,6 +7,14 @@ class PostList extends React.Component {
     this.props.fetchPosts()
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        //return blob of JSX
+      )
+    })
+  }
+
   render() {
     console.log(this.props.posts)
     return (
@@ -22,15 +30,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { fetchPosts })(PostList)
-
-//there are technically 2 console logs in browser console
-//this is because 1. when app is first loaded, all reducers run 1 initial time,
-//which will return a value of state as an empty array
-//then, during that initial app run, the render method will be called which invokes console log
-
-//2. immediately after PostList shows on screen, componentDidMount() will be called
-//and will go through the process of fetching data
-//after data is returned and an action dispatched to reducer,
-//the reducer will see the case and return that value of the action's payload property
-//Redux will see a new/different object value and return that new payload
-//mapStateToProps() will be call a 2nd time which will cal render() again with the 2nd console log
