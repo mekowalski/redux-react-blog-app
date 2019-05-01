1e04d68c6cf14f568f4ba5e6e1d1d0130131dbd5
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-01 15:08:13 -0600

    user .find to select the specific user from array

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,6 +8,10 @@ class UserHeader extends React.Component {
   }
 
   render() {
+    //component will have access to prop of this.props.users, array of all the users
+    //.find is a built-in method for JS arrays
+    //this will find just the user i care about
+    const user = this.props.users.find((user) => user.id === this.props.userId)
     return (
       <div>
         This is a Username
@@ -17,7 +21,7 @@ class UserHeader extends React.Component {
 }
 
 //this component needs access to Redux-level state
-//look at state.user and find a specific user
+//look at state.users and find a specific user
 const mapStateToProps = state => {
   return { users: state.users }
 }
