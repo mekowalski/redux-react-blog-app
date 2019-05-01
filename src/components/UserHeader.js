621cdcd18e3c0177efaf25f45814dff01fd30e4e
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-01 14:41:40 -0600

    create and add UserHeader component

---
+++ src/components/UserHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        This is a Username
+      </div>
+    )
+  }
+}
+export default UserHeader
