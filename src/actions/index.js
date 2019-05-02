9c768aa7d14989fc5ebff230e55a6a27f8c7acf2
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-02 14:30:40 -0600

    explain the 2nd way of memoize that still doesnt work correctly

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,11 +18,15 @@ export const fetchPosts = () => {
 //   }
 // }
 
-
-export const fetchUser = _.memoize(function(id) {
-  return async function(dispatch) {
+//second memoize attempt
+//memoize the inner function, that gets invoked with redux-thunk
+//this is still making 100 requests for 10 users
+//everytime this action creator is called, a new function is declared every time
+//the function is recreated, memoized and returned
+export const fetchUser = function(id) {
+  return _.memoize(async function(dispatch) {
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
-  }
-})
+  })
+}
